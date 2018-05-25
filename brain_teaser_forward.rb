85d9f0e1d36ddc519c318794c4f8df5d19702ef6
Clayton Crockett <claytoncrockett@gmail.com>
2018-05-25 12:19:28 -0600

    added recursive method for ruby

--- brain_teaser_forward.rb
+++ brain_teaser_forward.rb
@@ -1,4 +1,8 @@
-def brain_teaser(word)
+def brain_teaser()
+  word = gets.strip
+  if word == 'Q'
+    exit
+  end
   a = 0
   b = 0
   while a < word.length
@@ -12,4 +16,25 @@ def brain_teaser(word)
   end
 end
 
-brain_teaser("dog")
+def brain_teaser_recursive(word)
+  if word.length == 0
+    exit
+  end
+  spotInWord = 0
+  while spotInWord < word.length
+    print (word[0..spotInWord])
+    print (", ")
+    spotInWord+=1
+  end
+  x = word.split("")
+  x.shift
+  word = x.join("")
+  brain_teaser_recursive(word)
+end
+
+def getWord()
+  word = gets.strip
+  brain_teaser_recursive(word)
+end
+
+getWord()
